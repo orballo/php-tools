c814759efd06f272a1c87a01719ba6069fb76a1a
Eduardo Campaña <hi@orballo.dev>
2024-07-18 14:13:34 +0200

    wip

--- packages/parser/src/main.rs
+++ packages/parser/src/main.rs
@@ -3,13 +3,13 @@ use pest_derive::Parser;
 use std::fs;
 
 #[derive(Parser)]
-#[grammar = "grammar/base.pest"]
-pub struct CSVParser {}
+#[grammar = "grammar.pest"]
+pub struct PHPParser {}
 
 pub fn main() {
     let file_content = fs::read_to_string("packages/parser/src/fixtures/hello-world.php")
         .expect("Failed to read the file");
-    let file = CSVParser::parse(Rule::FILE, &file_content).expect("Failed to parse");
+    let file = PHPParser::parse(Rule::FILE, &file_content).expect("Failed to parse");
 
     eprintln!("file = {:#?}", file);
 }
